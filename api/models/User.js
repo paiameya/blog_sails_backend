ba91165bdaca1c63eaa11a3e1799fcb57a8e3f79
Jyothi-P-M <jyothi@socialsurvey.com>
2021-05-12 11:16:21 +0530

    setting db name,

--- api/models/User.js
+++ api/models/User.js
@@ -8,42 +8,25 @@
 module.exports = {
 
   attributes: {
-    attributes: {
-      name: { type: 'string', required: true },
-      passwordHash: { type: 'string', required: true },
-      email: { type: 'string', required: true },
-      sessions: {
-        collection: 'session',
-        via: "user"
-      },
-      blogs: {
-        collection: 'blog',
-        via: "user"
-      },
-      profile: {
-        collection: 'profile',
-        via: 'user'
-      },
-      comments: {
-        collection: 'profile',
-        via: 'user'
-      }
-
-      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    name: { type: 'string', required: true },
+    passwordHash: { type: 'string', required: true },
+    email: { type: 'string', required: true },
+    sessions: {
+      collection: 'session',
+      via: "user"
     },
+    blogs: {
+      collection: 'blog',
+      via: "user"
+    },
+    profile: {
+      collection: 'profile',
+      via: 'user'
+    },
+    comments: {
+      collection: 'profile',
+      via: 'user'
+    }
 
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
