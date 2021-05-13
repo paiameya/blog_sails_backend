7a5767c6bc35c6214aa198a2c569b71be1031750
jishnu007 <jishnupavithran007@gmail.com>
2021-05-13 17:47:46 +0530

    sample commit

--- api/controllers/BlogController.js
+++ api/controllers/BlogController.js
@@ -5,29 +5,50 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const Category = require("../models/Category");
+
 module.exports = {
     getBlogDetails: async function (req, res) {
         //API  to fetch blogs 
         //QueryParams: category,author,sortby,sortorder,limit,offset
         //Responce : Blog list
-        const { category, offset = 0, limit = 5, author, sortBy, sortOrder = "ASC" } = req.query;
-        let userId, categoryId, result;
-        if (category) {
-            const queryCategory = await Category.find({ name: category });
-            categoryId = queryCategory.flatMap(eachCategory => eachCategory.id);
+        try {
+            let { category, offset = 0, limit = 5, author, sortBy, sortOrder = "ASC" } = req.query;
+            let userId, categoryId = [], result;
+            console.log("...category", category)
+            if (category && !Array.isArray(category)) { category = [category] }
+            console.log("category...", category)
+            if (category) {
+                var categoryItem, queryCategory
+                for (const eachCategory of category) {
+                    categoryItem = await Category.find({ name: eachCategory })
+                    categoryId.push(categoryItem.id)
+                }
+                console.log(categoryId)
+
+                // categoryId = queryCategory.flatMap(eachCategory => eachCategory.id);
+            }
+
+            if (author) {
+                const queryAuthor = await User.find({ name: author });
+                userId = queryAuthor.flatMap(eachAuthor => eachAuthor.id)
+            }
+
+            if (sortBy)
+                result = await Blog.find({ 'category': categoryId, 'user': userId }).skip(offset).limit(limit).sort(`${sortBy} ${sortOrder}`)
+            else
+                result = await Blog.find({ 'category': categoryId, 'user': userId }).skip(offset).limit(limit)
+
+            if (!result) { throw 'notFound'; }
+
+            return res.status(200).json({ responce: result });
         }
-        if (author) {
-            const queryAuthor = await User.find({ name: author });
-            userId = queryAuthor.flatMap(eachAuthor => eachAuthor.id)
+        catch (err) {
+            return res
+                .status(500)
+                .send(
+                    'We are currently unable to process your request. Please try after sometime.'
+                )
         }
-
-        if (sortBy)
-            result = await Blog.find({ 'category': categoryId, 'user': userId }).skip(offset).limit(limit).sort(`${sortBy} ${sortOrder}`)
-        else
-            result = await Blog.find({ 'category': categoryId, 'user': userId }).skip(offset).limit(limit)
-
-        if (!result) { throw 'notFound'; }
-
-        return res.status(200).json({ responce: result });
     }
 };
