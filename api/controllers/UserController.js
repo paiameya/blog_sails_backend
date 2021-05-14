dbe35c845986432e69fb71801c8dcf3223068bb2
Jyothi-P-M <jyothi@socialsurvey.com>
2021-05-14 12:45:34 +0530

    api to fetch author details

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,4 +5,25 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-module.exports = {};
+
+module.exports = {
+    authorDetails: async function (req, res) {
+        try {
+            let { id } = req.params
+            if (id) {
+                let authorDetails = await User.find({ where: { id: id }, select: ['name', 'email'] }).populate('profile')
+                if (authorDetails) {
+                    delete authorDetails[0].salt
+                    res.status(200).send(authorDetails[0])
+                }
+                else {
+                    res.status(409).send("Author not found")
+                }
+            }
+        }
+        catch (err) {
+            console.log("err", err)
+            res.status(500).json("Something went wrong")
+        }
+    }
+};
