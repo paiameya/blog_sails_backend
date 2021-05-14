c93a2d6f26ec1b73e0f5b9139133bf4d971ba81b
Jyothi-P-M <jyothi@socialsurvey.com>
2021-05-14 15:16:21 +0530

    api to fetch author details

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -10,11 +10,15 @@ module.exports = {
     authorDetails: async function (req, res) {
         try {
             let { id } = req.params
+            let responseObj = {}
             if (id) {
-                let authorDetails = await User.find({ where: { id: id }, select: ['name', 'email'] }).populate('profile')
+                let authorDetails = await User.findOne({ where: { id: id }, select: ['name', 'email'] }).populate('profile')
                 if (authorDetails) {
-                    delete authorDetails[0].salt
-                    res.status(200).send(authorDetails[0])
+                    let title = authorDetails.profile?.title || null
+                    let profilePicture = authorDetails.profile?.profilePicture || null
+                    delete authorDetails.profile
+                    responseObj = { ...authorDetails, title, profilePicture }
+                    res.status(200).send(responseObj)
                 }
                 else {
                     res.status(409).send("Author not found")
