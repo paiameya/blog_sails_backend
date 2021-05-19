e42bf87db753458ccecf61496d3a754a5618ec06
Jyothi-P-M <jyothi@socialsurvey.com>
2021-05-19 08:58:22 +0530

    merge develop nto logout

--- api/controllers/auth/logout.js
+++ api/controllers/auth/logout.js
@@ -7,11 +7,13 @@ module.exports = {
 
   exits: {},
 
-  fn: async function () {
-    const session = await Session.update({
-      sessionToken: this.req.sessionToken,
-      status: 1
-    }).set({ status: 0 });
-    if (session) return 'Successfully Logged Out';
+
+
+
+  fn: async function (inputs) {
+
+    let session = await Session.update({ sessionToken: this.req.sessionToken, status: 1 }).set({ status: 0 })
+    if (session)
+      return this.ref.status(200).send("Successfully logged out")
   }
 };
--- api/policies/is-logged-in.js
+++ api/policies/is-logged-in.js
@@ -1,14 +1,13 @@
 module.exports = async function (req, res, proceed) {
-  const token = req.get('authorization');
-  if (token === undefined) return res.forbidden();
-  const session = await Session.findOne({
-    sessionToken: token,
-    status: 1
-  }).populate('user');
-  if (session && session.user !== null) {
-    req.me = session.user;
-    req.sessionToken = token;
-    return proceed();
-  }
-  return res.forbidden();
+    let token = req.get("authorization")
+    console.log("token", token)
+    if (token === undefined) return res.forbidden();
+    let session = await Session.find({ sessionToken: token, status: 1 }).populate('user')
+    console.log("session", session)
+    if (session && session.length && session.user !== null) {
+        req.me = session.user
+        req.sessionToken = token
+        return proceed();
+    }
+    return res.forbidden();
 };
